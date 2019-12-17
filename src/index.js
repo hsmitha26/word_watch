3216ed702dd4c45fa1e37e92c681ae1ce586e603
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-12-17 11:34:55 -0700

    successfully displays top word and count

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
-})
+let apiUrl = 'https://wordwatch-api.herokuapp.com/api/v1/top_word'
+  $.ajax({
+      url: apiUrl,
+      context: document.body,
+      success: function(result){
+        let topWord = Object.keys(result.word)[0]
+        let topWordCount = Object.values(result.word)[0]
+        $('.word-count').html(`${topWord}: ${topWordCount}`)
+      }
+  })
+});
